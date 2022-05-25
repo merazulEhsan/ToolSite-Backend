4ab7bd26b6f946f02c7ccdf1bdd52155e4e87108
merazul2755 <merazulehsan880@gmail.com>
2022-05-25 22:16:24 +0600

    Product api

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const cors = require('cors')
 require('dotenv').config();
@@ -7,8 +8,35 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xhui8i0.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+  try{
+
+    await client.connect(); 
+    const productCollection = client.db('Tool_Site').collection('Products')
+
+    app.get('/products', async(req,res)=>{
+      const query = {};
+      const cursour = productCollection.find(query);
+      const products = await cursour.toArray();
+      res.send(products);
+    })
+
+  }finally{
+
+  }
+
+}
+run().catch(console.dir);
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('WELCOME TO TOOLSITE!')
 })
 
 app.listen(port, () => {
